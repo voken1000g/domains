1ceb51ad9e272e543f8084b4caa2214d3e3aaea8
Lyman Snow <pmvoken@protonmail.com>
2021-02-24 05:59:56 +0000

    added: text_link/scheme pattern

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from urllib import request
 from bs4 import BeautifulSoup
 
-ALPHA_PATTERN = re.compile(r'^.[a-z]+$')
+ALPHA_PATTERN = re.compile(r'[a-z]')
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
@@ -21,13 +21,20 @@ def touch_dir(path_to_dir):
 
 def main():
     domains = []
-    domains_full = []
     domains_alpha = []
+
+    names = []
+    names_alpha = []
+
+    # domains_full = []
+    # names_full = []
+
     readme = [
         '# All the world’s top-level domains',
         '',
         'Updated at {}, from [IANA Root Zone Database](http://www.iana.org/domains/root/db)'.format(
-            datetime.utcnow().isoformat()),
+            datetime.utcnow().isoformat()
+        ),
         '',
         '### domains',
         '',
@@ -58,92 +65,109 @@ def main():
             tds = tr.find_all('td')
 
             str_domain = tds[0].span.a.get_text()
+            str_name = str_domain.strip('.')
             str_href = tds[0].span.a.get('href')
             str_type = tds[1].get_text()
             str_tld_manager = tds[2].get_text().replace('\n', '')
 
             domains.append(str_domain)
-            domains_full.append(str_domain)
+            names.append(str_name)
+            # domains_full.append(str_domain)
+            # names_full.append(str_domain.strip('.'))
 
-            if re.match(ALPHA_PATTERN, str_domain):
-                readme.append('| {} | {} | {} | [Details](https://www.iana.org{}) |'.format(str_domain,
-                                                                                            str_type,
-                                                                                            str_tld_manager,
-                                                                                            str_href))
+            readme.append('| {} | {} | {} | [Details](https://www.iana.org{}) |'.format(str_domain,
+                                                                                        str_type,
+                                                                                        str_tld_manager,
+                                                                                        str_href))
+
+            if re.match(ALPHA_PATTERN, str_name):
                 domains_alpha.append(str_domain)
+                names_alpha.append(str_domain.strip('.'))
 
-            else:
-                punycode = str_domain.replace('.', '').encode('punycode').decode('utf-8')
-                str_domain_punycode = '.xn--{}'.format(punycode)
-                readme.append('| {}<br>{} | {} | {} | [Details](https://www.iana.org{}) |'.format(
-                    str_domain,
-                    str_domain_punycode,
-                    str_type,
-                    str_tld_manager,
-                    str_href)
-                )
-                domains_full.append(str_domain_punycode)
-                domains_alpha.append(str_domain_punycode)
-
-    # domains
-    with codecs.open(os.path.join(DATA_DIR, 'domains.txt'), 'w', encoding='utf-8') as f:
-        content = '\n'.join(domains)
-        f.write(content)
-    with codecs.open(os.path.join(DATA_DIR, 'domains.json'), 'w', encoding='utf-8') as f:
-        json.dump(domains, f)
-
-    # domains_full
-    with codecs.open(os.path.join(DATA_DIR, 'domains_full.txt'), 'w', encoding='utf-8') as f:
-        content = '\n'.join(domains_full)
-        f.write(content)
-    with codecs.open(os.path.join(DATA_DIR, 'domains_full.json'), 'w', encoding='utf-8') as f:
-        json.dump(domains_full, f)
-
-    # domains_alpha
-    with codecs.open(os.path.join(DATA_DIR, 'domains_alpha.txt'), 'w', encoding='utf-8') as f:
-        content = '\n'.join(domains_alpha)
-        f.write(content)
-    with codecs.open(os.path.join(DATA_DIR, 'domains_alpha.json'), 'w', encoding='utf-8') as f:
-        json.dump(domains_alpha, f)
-
-    # text link pattern
-    re_list = []
-    for domain in domains:
-        re_list.append('\\' + domain)
-
-    text_link_pattern = re.compile(
-        r'(?:\.?[a-zA-Z0-9_-]+)+(?:' + '|'.join(re_list) + ')(?::[0-9]+)*(?:/[a-zA-Z0-9_/?&=#%~!@*(),:;+.-]+|/)*'
-    )
-    with open(os.path.join(DATA_DIR, 'text_link_pattern.pickle'), 'wb') as f:
-        pickle.dump(text_link_pattern, f)
-
-    # text link pattern (full)
-    re_full_list = []
-    for domain in domains_full:
-        re_full_list.append('\\' + domain)
-
-    text_link_pattern_full = re.compile(
-        r'(?:\.?[a-zA-Z0-9_-]+)+(?:' + '|'.join(re_full_list) + ')(?::[0-9]+)*(?:/[a-zA-Z0-9_/?&=#%~!@*(),:;+.-]+|/)*'
-    )
-    with open(os.path.join(DATA_DIR, 'text_link_pattern_full.pickle'), 'wb') as f:
-        pickle.dump(text_link_pattern_full, f)
-
-    # text link pattern (alpha)
-    re_alpha_list = []
-    for domain in domains_alpha:
-        re_alpha_list.append('\\' + domain)
-
-    text_link_pattern_alpha = re.compile(
-        r'(?:\.?[a-zA-Z0-9_-]+)+(?:' + '|'.join(re_alpha_list) + ')(?::[0-9]+)*(?:/[a-zA-Z0-9_/?&=#%~!@*(),:;+.-]+|/)*'
-    )
-    with open(os.path.join(DATA_DIR, 'text_link_pattern_alpha.pickle'), 'wb') as f:
-        pickle.dump(text_link_pattern_alpha, f)
+            # else:
+            #     name_punycode = 'xn--' + str_domain.strip('.').encode('punycode').decode('utf-8')
+            #     domain_punycode = '.' + name_punycode
+            #
+            #     readme.append('| {}<br>{} | {} | {} | [Details](https://www.iana.org{}) |'.format(
+            #         str_domain,
+            #         domain_punycode,
+            #         str_type,
+            #         str_tld_manager,
+            #         str_href)
+            #     )
+            #     domains_full.append(domain_punycode)
+            #     domains_alpha.append(domain_punycode)
+            #
+            #     names_full.append(name_punycode)
+            #     names_alpha.append(name_punycode.upper())
 
     # README.md
     readme.append('')
     with codecs.open('README.md', 'w', encoding='utf-8') as f:
         content = '\n'.join(readme)
         f.write(content)
+
+    # domains
+    with codecs.open(os.path.join(DATA_DIR, 'domains.txt'), 'w', encoding='utf-8') as f:
+        f.write('\n'.join(domains))
+    with codecs.open(os.path.join(DATA_DIR, 'domains.json'), 'w', encoding='utf-8') as f:
+        json.dump(domains, f)
+
+    # names
+    with codecs.open(os.path.join(DATA_DIR, 'names.txt'), 'w', encoding='utf-8') as f:
+        f.write('\n'.join(names))
+    with codecs.open(os.path.join(DATA_DIR, 'names.json'), 'w', encoding='utf-8') as f:
+        json.dump(names, f)
+
+    # domains_alpha
+    with codecs.open(os.path.join(DATA_DIR, 'domains_alpha.txt'), 'w', encoding='utf-8') as f:
+        f.write('\n'.join(domains_alpha))
+    with codecs.open(os.path.join(DATA_DIR, 'domains_alpha.json'), 'w', encoding='utf-8') as f:
+        json.dump(domains_alpha, f)
+
+    # names_alpha
+    with codecs.open(os.path.join(DATA_DIR, 'names_alpha.txt'), 'w', encoding='utf-8') as f:
+        f.write('\n'.join(names_alpha))
+    with codecs.open(os.path.join(DATA_DIR, 'names_alpha.json'), 'w', encoding='utf-8') as f:
+        json.dump(names_alpha, f)
+
+    # # domains_full
+    # with codecs.open(os.path.join(DATA_DIR, 'domains_full.txt'), 'w', encoding='utf-8') as f:
+    #     content = '\n'.join(domains_full)
+    #     f.write(content)
+    # with codecs.open(os.path.join(DATA_DIR, 'domains_full.json'), 'w', encoding='utf-8') as f:
+    #     json.dump(domains_full, f)
+
+    names.reverse()
+    names_alpha.reverse()
+
+    # text link pattern
+    text_link_pattern = re.compile(
+        r'(?:[-a-zA-Z0-9]+\.)+(?:' + '|'.join(names) + ')(?::[0-9]+)*(?:/[-a-zA-Z0-9_/?&=#%~!@*(),:;+.]+|/)*'
+    )
+    with open(os.path.join(DATA_DIR, 'text_link_pattern.pickle'), 'wb') as f:
+        pickle.dump(text_link_pattern, f)
+
+    # text link pattern (alpha)
+    text_link_pattern_alpha = re.compile(
+        r'(?:[-a-zA-Z0-9]+\.)+(?:' + '|'.join(names_alpha) + ')(?::[0-9]+)*(?:/[-a-zA-Z0-9_/?&=#%~!@*(),:;+.]+|/)*'
+    )
+    with open(os.path.join(DATA_DIR, 'text_link_pattern_alpha.pickle'), 'wb') as f:
+        pickle.dump(text_link_pattern_alpha, f)
+
+    # scheme pattern
+    scheme_pattern = re.compile(
+        r'^((?:[-a-zA-Z0-9]+\.)+(?:' + '|'.join(names) + '))'
+    )
+    with open(os.path.join(DATA_DIR, 'scheme_pattern.pickle'), 'wb') as f:
+        pickle.dump(scheme_pattern, f)
+
+    # scheme pattern (alpha)
+    scheme_pattern_alpha = re.compile(
+        r'^((?:[-a-zA-Z0-9]+\.)+(?:' + '|'.join(names_alpha) + '))'
+    )
+    with open(os.path.join(DATA_DIR, 'scheme_pattern_alpha.pickle'), 'wb') as f:
+        pickle.dump(scheme_pattern_alpha, f)
 
 
 if __name__ == "__main__":
